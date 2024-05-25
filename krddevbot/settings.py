816bbe6a6a1d670285e2e573bdacc089642dc1ff
DrozdovNikolai <kolya.drozdov.20031@mail.ru>
2024-05-25 03:19:42 +0300

    123

--- krddevbot/settings.py
+++ krddevbot/settings.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-LOG_LEVEL = "INFO"
-SENTRY_DSN =  os.getenv('sentry_dsn')
-LOLS_BOT_ENABLED = bool("True")
-EMOJI_TIMEOUT_SECONDS = int("60")
-GARBAGE_MESSAGE_TIMEOUT_SECONDS = int("30")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+LOLS_BOT_ENABLED = bool(os.environ.get("LOLS_BOT_ENABLED", "True"))
+EMOJI_TIMEOUT_SECONDS = int(os.environ.get("EMOJI_TIMEOUT_SECONDS", "60"))
+GARBAGE_MESSAGE_TIMEOUT_SECONDS = int(os.environ.get("GARBAGE_MESSAGE_TIMEOUT_SECONDS", "30"))
